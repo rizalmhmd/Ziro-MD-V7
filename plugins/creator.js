4f34144ab6aae940fd4968c8843868d557033340
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-05 17:56:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,13 +5,13 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
 if (command == 'creator') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Ziro-MD\nNICKNAME:👑 Owner 𝑍𝐼𝑅𝑂-𝑀𝐷\nORG: 𝑍𝐼𝑅𝑂-𝑀𝐷\nTITLE:soft\nitem1.TEL;waid=6281228070013:+62 812-2807-0013\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/Cerdasin62\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: ziro-md_official@gmail.com\nitem3.X-ABLabel:💌 Mail Owner ZiroBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 ,02 Fabruari 2004\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; 𝚉𝚊𝚕𝚕\nNICKNAME:👑 Owner 𝙕𝘼𝙇𝙇𝙭𝘽𝙊𝙏𝙕\nORG: 𝙕𝘼𝙇𝙇𝙭𝘽𝙊𝙏𝙕\nTITLE:soft\nitem1.TEL;waid=6281333154367:+62 813-3315-4367\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/Cerdasin62\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: payahhawok@gmail.com\nitem3.X-ABLabel:💌 Mail Owner 𝙕𝘼𝙇𝙇𝙭𝘽𝙊𝙏𝙕\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 ,PRIVATE\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 let caption = `👋 Hai *@${who.split("@")[0]}*, Nih Owner Saya kak`
     await conn.sendButton(m.chat, caption, author, null, [['👨Sapa Owner', 'Huuu']], m, { quoted: fkontak, mentions: conn.parseMention(caption) })
 }
 if (command == 'own') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6283863727401:+62 838-6372-7401\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6281392128956:+62 813-9212-8956\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya `,m)
   }
